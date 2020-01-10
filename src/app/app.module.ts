5f1fd8ff5f57a1d221db70b1b00d7498899719d3
Runqi Luo <richie.runqi@gmail.com>
2020-01-10 11:26:41 -0500

    rebuild outline, structure and components cleared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MaterialComponent } from './material/material.component';
 import { ResultComponent } from './result/result.component';
+import { CurrentComponent } from './current/current.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MaterialComponent,
     ResultComponent,
+    CurrentComponent,
   ],
   imports: [
     BrowserModule,
