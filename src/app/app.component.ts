d3cdf18fcd130c3fa4f146b82b63211fa91f8a14
Runqi Luo <richie.runqi@gmail.com>
2020-01-10 14:46:27 -0500

    first finished version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,72 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Welcome to Angular';
-  changeableName = 'myName';
+  materials = [];
+  mes:string;
+  egg(){
+    if(this.materials.includes("鸡蛋")){
+      return;
+    }
+    this.materials.push("鸡蛋");
+  }
+
+  tomato(){
+    if(this.materials.includes("番茄")){
+      return;
+    }
+    this.materials.push("番茄");
+  }
+
+  grounded(){
+    if(this.materials.includes("肉糜")){
+      return;
+    }
+    this.materials.push("肉糜");
+  }
+
+  carrot(){
+    if(this.materials.includes("胡萝卜")){
+      return;
+    }
+    this.materials.push("胡萝卜");
+  }
+
+  belly(){
+    if(this.materials.includes("五花肉")){
+      return;
+    }
+    this.materials.push("五花肉");
+  }
+
+  garlic(){
+    if(this.materials.includes("蒜苗")){
+      return;
+    }
+    this.materials.push("蒜苗");
+  }
+
+  cancook(){
+    if(this.materials.includes("鸡蛋") && this.materials.includes("番茄")){
+      this.mes="番茄炒蛋完成！";
+      return;
+    }
+    if(this.materials.includes("鸡蛋") && this.materials.includes("肉糜")){
+      this.mes="肉饼炖蛋完成！";
+      return;
+    }
+    if(this.materials.includes("胡萝卜") && this.materials.includes("蒜苗") && this.materials.includes("五花肉")){
+      this.mes="回锅肉完成！";
+      return;
+    }
+    if(this.materials.length > 0){
+      this.mes="黑暗料理完成！";
+    }
+  }
+
+  reset(){
+    this.materials=[];
+    this.mes="";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-import { MaterialComponent } from './material/material.component';
-import { ResultComponent } from './result/result.component';
-import { CurrentComponent } from './current/current.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MaterialComponent,
-    ResultComponent,
-    CurrentComponent,
   ],
   imports: [
     BrowserModule,
